5f1e7da1afcebf80a46cef0269a8fdb3f23bec29
unknown <shumonshihab62@gmail.com>
2021-10-18 00:18:02 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWolFhaFPsOa0Zo2oIt2c56DJxekTJpgA",
+    authDomain: "ayurvedic-care-f247a.firebaseapp.com",
+    projectId: "ayurvedic-care-f247a",
+    storageBucket: "ayurvedic-care-f247a.appspot.com",
+    messagingSenderId: "129452636320",
+    appId: "1:129452636320:web:72784f5a7f35db16be71c5"
+};
+
+export default firebaseConfig;
